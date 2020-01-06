335ada92fab93a70e38631ba8d38399d4096db3c
Mark <mark@murphymark.me>
2020-01-05 21:01:16 -0600

    delete and filter tasks

--- app.js
+++ app.js
@@ -7,13 +7,19 @@ const taskInput = document.querySelector('#task');
 
 // Load all event listeners
 function loadEventListeners() {
-// Add task event
-form.addEventListener('submit', addTask);
+    // Add task event
+    form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
 function addTask(e) {
-    if(taskInput.value === '') {
+    if (taskInput.value === '') {
         alert('Add a task');
     }
 
@@ -39,6 +45,38 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are You Sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear Tasks
+function clearTasks(e) {
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(
+        function(task){
+            const item= task.firstChild.textContent;
+            if(item.toLowerCase().indexOf(text) != -1){
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            }
+        }
+    );
 }
 
 // Load all event listeners
